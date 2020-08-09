d1728fae927e564671d2a9f1514c6119ad9ff4e7
PatarFen <37533674+PatarFen@users.noreply.github.com>
2020-08-09 17:41:34 +0800

    Add files via upload

--- 0004.py
+++ 0004.py
@@ -1,15 +1,16 @@
 import re
 
-f = open("4.txt","r")
+#f = open("4.txt","r") 
+#using with is more secure to open file
+with open("4.txt","r") as f:
+    content_lines = f.readlines()
 
-content_lines = f.readlines()
+    words_set = set()
 
-words_set = set()
-
-for i in content_lines:
-    words = re.split('\W+',i)
-    for j in words:
-        if j != '':
-            words_set.add(j)
+    for i in content_lines:
+        words = re.split('\W+',i)
+        for j in words:
+            if j != '':
+                words_set.add(j)
 
 print(len(words_set))
